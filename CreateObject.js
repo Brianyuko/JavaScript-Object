437abc98fe9629b10c8e6265ccaa282fa1374541
Brianyuko <brianyuko45@gmail.com>
2020-06-22 01:45:21 +0700

    Set Data

--- CreateObject.js
+++ CreateObject.js
@@ -2,7 +2,13 @@
 var programmer = {
     nama    : "Erwin",
     umur    : 20,
-    hobi   : "Photographer"
+    hobi    : "Photographer",
+    getData : function(){
+        return "Nama: " + this.nama + "<br>" + "Umur: " + this.umur;
+    },
+    setData: function(nama){
+        this.nama = nama;
+    }
 };
 
 // Create Object Option Two
@@ -11,9 +17,13 @@ designer.nama = "Alex";
 designer.umur = 40;
 designer.hobi = "Koki"
 
+// Set Object
+programmer.setData('Brian')
+
 // Get Object Option One
 document.getElementById('umur').innerHTML = programmer.umur;
 document.getElementById('hobi').innerHTML = programmer.hobi;
 
 // Get Object Option Two
 document.getElementById('nama').innerHTML = designer["nama"];
+document.getElementById('box').innerHTML = programmer.getData();
